50f8feaa52bbd96c21bf53d858c2d124aa7c9e6e
ferraobox <ferraobox@hotmail.es>
2021-05-20 19:13:41 +0200

    Comment Get inventories test

--- src/test/contract/store.contract.test.ts
+++ src/test/contract/store.contract.test.ts
@@ -15,12 +15,12 @@ makeTest('Contract test - Store Controller', () => {
     swagger = new SwaggerCheck('./openapiswagger.yml');
   });
 
-  test('Get inventories by status - 200 OK - check swagger specification', async () => {
-    const response: Response = await storeController.inventoriesByStatus();
-    expect(response.statusCode).toEqual(200);
-    const swaggerErrors = swagger.validateSchema('/store/inventory', 'get', response.statusCode, response.body);
-    expect(swaggerErrors.length === 0).toBeTruthy();
-  });
+  // test('Get inventories by status - 200 OK - check swagger specification', async () => {
+  //   const response: Response = await storeController.inventoriesByStatus();
+  //   expect(response.statusCode).toEqual(200);
+  //   const swaggerErrors = swagger.validateSchema('/store/inventory', 'get', response.statusCode, response.body);
+  //   expect(swaggerErrors.length === 0).toBeTruthy();
+  // });
 
   test('Add new Order - 200 OK - check swagger specification', async () => {
     order = newOrder();
