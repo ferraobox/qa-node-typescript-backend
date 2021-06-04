b8ca32e708c08b109405e22ead6036c4fca72edf
ferraobox <ferraobox@hotmail.es>
2021-06-04 15:08:49 +0200

    Fix workflow release changes on runDocker.sh

--- setup/runDocker.sh
+++ setup/runDocker.sh
@@ -1,3 +1,5 @@
  #!/bin/bash
+docker pull swaggerapi/petstore3:latest >> dockerpull.log
+rm dockerpull.log 2> /dev/null
 dockerID=$(docker run  --name swaggerapi-petstore3 -d -p 8080:8080 swaggerapi/petstore3:latest)
 echo $dockerID
